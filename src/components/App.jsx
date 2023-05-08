6f9844d138c94f5e86f542cdf15d63d9014ffc05
K-Basket <koshykkv@gmail.com>
2023-05-09 00:08:02 +0300

    add Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  formSubmitHandler = data => {
+    console.log('data', data);
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitData={this.formSubmitHandler} />
+
+        <h2>Contacts</h2>
+        {/* <Filter /> */}
+        {/* <ContactList /> */}
+      </>
+    );
+  }
+}
